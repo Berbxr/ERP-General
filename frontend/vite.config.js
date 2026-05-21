a9288278424fb718285e62471f7b3a57b43c95a5
Berbxr <a20490714@itmexicali.edu.mx>
2026-05-20 17:24:16 -0700

    Cambio de host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['demo3.inetmxli.com', '66.179.243.135'],
+    allowedHosts: ['demo9.inetmxli.com', '66.179.243.135'],
 
     hmr: {
-    host: 'demo3.inetmxli.com',  // Tu dominio
+    host: 'demo9.inetmxli.com',  // Tu dominio
     protocol: 'wss',              // Usa 'ws' (o 'wss' si ya tienes certificado SSL/candadito)
     clientPort: 443,              // Usa 80 para http (o 443 si usas https)
   },
